2582c861436048425e08c9fee6ac5193c17fe9de
strujilloz <stztrujillo@hotmail.com>
2022-12-19 06:53:42 +0000

    Both scripts done to deploy and interact with the contract

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -2,11 +2,26 @@
 const hre = require("hardhat");
 
 async function main() {
+  console.log("Getting contracts from factory")
+  const Exchange = await hre.ethers.getContractFactory("Exchange")
   const Token = await hre.ethers.getContractFactory("Token");
-  const token = await Token.deploy()
-  await token.deployed()
 
-  console.log(`Token deployed to: ${token.address}`);
+  const [deployer, acc1, acc2] = await hre.ethers.getSigners()
+  console.log(deployer.address)
+  
+  console.log("Deploying contracts")
+  const exchange = await Exchange.deploy(deployer.address, 10)
+  const tokenUSDX = await Token.deploy("Usdx Token", "USDX", 1000)
+  const tokenBCK = await Token.deploy("Blockchain Token", "BCK", 1000)
+  await exchange.deployed()
+  await tokenUSDX.deployed()
+  await tokenBCK.deployed()
+  
+
+  console.log(`TokenBCK deployed to: ${tokenBCK.address}`);
+  console.log(`TokenUSDX deployed to: ${tokenUSDX.address}`);
+  console.log(`Exchange deployed to: ${exchange.address}`);
+ 
 }
 
 
